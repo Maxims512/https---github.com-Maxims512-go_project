4311fcea718e55204325bf304211de6b1113d13b
maximix <ivan82kaktus@mail.ru>
2023-08-16 14:17:16 +0400

    написал функцию получения вопроса

--- main.go
+++ main.go
@@ -3,7 +3,7 @@ package main
 import (
 	"fmt"
 	"os"
-	//"strconv"
+	"math/rand"
 )
 
 func openFile() string {
@@ -15,7 +15,6 @@ func openFile() string {
 func give_vopr_otv(text string) (array[100]rune) {
 	var schet int
 	var perem int
-	//var otvet rune
 	for _, i := range text {
 		if i == 13 || i == 10{
 			schet++	
@@ -32,13 +31,34 @@ func give_vopr_otv(text string) (array[100]rune) {
 	return array
 }
 
-//func give_vopr(text string) (array[100]string)
+func give_vopr(text string) (array[100]string){
+	var schet int
+	var vopr string
+	var perem int
+	for _, i := range text {
+		if schet<8 {
+			vopr += string(i)
+		} else{
+			array[perem]=vopr
+			vopr=""
+			perem++
+			schet=-4
+		}
+		if i == 13 || i == 10{
+			schet++	
+		}
+	}
+	return array
+}
 
 
 func main() {
 	data := openFile()
-	vopr_otvet := give_vopr_otv(data)
-	for i := 0; i < 100; i++{
-        fmt.Println(i+1, string(vopr_otvet[i]))
-    }
+	//vopr_otvet := give_vopr_otv(data)
+	//for i := 0; i < 100; i++{
+       // fmt.Println(i+1, string(vopr_otvet[i]))
+    //}
+	vopr := give_vopr(data)
+	fmt.Println(vopr[1])
+	fmt.Print(rand.Intn(100))
 }
